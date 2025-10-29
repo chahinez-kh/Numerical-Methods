6521e4d7daee889bd6a89120dce2b2d7b66c3be6
chahinez-kh <chahinezkhireddine971@gmail.com>
2025-10-29 15:48:07 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+1) Report concepts (brief examples)
+- Variables and Data Types:
+  x = 5        # int
+  y = 2.5      # float
+  name = 'Shey' # str
+  is_valid = True # bool
+
+- Lists:
+  lst = [1, 2, 3]
+
+- Tuples:
+  t = (1, 2, 3)
+
+- Dictionaries:
+  d = {'a': 1, 'b': 2}
+
+- Sets:
+  s = {1, 2, 3}
+
+- Control Flow:
+  if x > 0:
+    print('positive')
+  for i in range(3):
+    print(i)
+
+- Functions:
+  def add(a, b):
+    return a + b
+
+- Modules and Packages:
+  import numpy as np
